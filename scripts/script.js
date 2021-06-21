0d5218c43d45b459766228fd557e1dc7c75cb8c3
vidhi kiran <vidhi.kiran.mec19@itbhu.ac.in>
2021-06-21 12:08:33 +0530

    adding new page for logging in

--- scripts/script.js
+++ scripts/script.js
@@ -40,6 +40,29 @@ var localStreams = {
   }
 };
 
+// show mute icon whenever a remote has muted their mic
+client.on("mute-audio", function (evt) {
+  toggleVisibility('#' + evt.uid + '_mute', true);
+});
+
+client.on("unmute-audio", function (evt) {
+  toggleVisibility('#' + evt.uid + '_mute', false);
+});
+
+// show user icon whenever a remote has disabled their video
+client.on("mute-video", function (evt) {
+  var remoteId = evt.uid;
+  // if the main user stops their video select a random user from the list
+  if (remoteId != mainStreamId) {
+    // if not the main vidiel then show the user icon
+    toggleVisibility('#' + remoteId + '_no-video', true);
+  }
+});
+
+client.on("unmute-video", function (evt) {
+  toggleVisibility('#' + evt.uid + '_no-video', false);
+});
+
 client.init("0adf5b14219840e69ab936e11b3e4465", function() {
     console.log("client initialized");
 }, function(err) {
@@ -103,6 +126,8 @@ function leaveChannel() {
     $("#remote-container").empty() // clean up the remote feeds
     //disable the UI elements
     $("#exit-btn").prop("disabled", true);
+    $("#mic-btn").prop("disabled", true);
+    $("#video-btn").prop("disabled", true);
     // hide the mute/no-video overlays
   }, function(err) {
     console.log("client leave failed ", err); //error handling
--- ui.js
+++ ui.js
@@ -1,9 +1,53 @@
 function enableUiControls(localStream) {
-
+  $("#mic-btn").prop("disabled", false);
+  $("#video-btn").prop("disabled", false);
   $("#exit-btn").prop("disabled", false);
 
+  $("#mic-btn").click(function(){
+    toggleMic(localStream);
+  });
+
+  $("#video-btn").click(function(){
+    toggleVideo(localStream);
+  });
+
   $("#exit-btn").click(function(){
-    alert("so sad to see you leave the channel");
+    alert("So sad to see you leave the channel!");
     leaveChannel();
   });
+}
+function toggleBtn(btn){
+  btn.toggleClass('btn-dark').toggleClass('btn-danger');
+}
+
+function toggleVisibility(elementID, visible) {
+  if (visible) {
+    $(elementID).attr("style", "display:block");
+  } else {
+    $(elementID).attr("style", "display:none");
+  }
+}
+
+function toggleMic(localStream) {
+toggleBtn($("#mic-btn")); // toggle button colors
+$("#mic-icon").toggleClass('fa-microphone').toggleClass('fa-microphone-slash'); // toggle the mic icon
+if ($("#mic-icon").hasClass('fa-microphone')) {
+  localStream.enableAudio(); // enable the local mic
+  toggleVisibility("#mute-overlay", false); // hide the muted mic icon
+} else {
+  localStream.disableAudio(); // mute the local mic
+  toggleVisibility("#mute-overlay", true); // show the muted mic icon
+}
+}
+
+function toggleVideo(localStream) {
+  toggleBtn($("#video-btn")); // toggle button colors
+  $("#video-icon").toggleClass('fa-video').toggleClass('fa-video-slash'); // toggle the video icon
+  if ($("#video-icon").hasClass('fa-video')) {
+    localStream.enableVideo(); // enable the local video
+    toggleVisibility("#no-local-video", false); // hide the user icon when video is enabled
+  } else {
+    localStream.disableVideo(); // disable the local video
+    toggleVisibility("#no-local-video", true); // show the user icon when video is disabled
+  }
 }
